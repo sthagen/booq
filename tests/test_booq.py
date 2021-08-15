72f06fb335e66882fcbd5a6943eb0ca86c4e0340
Stefan Hagen <stefan@hagen.link>
2021-08-15 20:59:01 +0200

    Added meta parsing code (most parts) to prototype (WIP)

--- tests/test_booq.py
+++ tests/test_booq.py
@@ -23,6 +23,68 @@ def load_events(path: pathlib.Path):
     return orjson.loads(event_data)
 
 
+def parse_meta(events):
+    """Parse the meta data from the events dictionary.
+
+    "_meta": {
+      "binary": {
+        "logic": "first",
+        "evaluation": [
+          "is_good",
+          "is_bad"
+        ]
+      },
+      "is_bad": [
+        true
+      ],
+      "is_good": [
+        {
+          "key": "closed",
+          "values": [
+            {
+              "is": true
+            }
+          ]
+        },
+        {
+          "key": "status",
+          "values": [
+            {
+              "eq": "MERGED"
+            }
+          ]
+        }
+      ],
+      "time_window": [
+        {
+          "key": "create_at"
+        },
+        {
+          "key": "close_at"
+        }
+      ]
+    }
+    """
+    algorithm = {
+        'order': events['_meta']['binary']['logic'],
+        'evaluation': events['_meta']['binary']['evaluation'],
+        'is_bad': events['_meta']['is_bad'][0],  # HACK A DID ACK
+        'is_good': events['_meta']['is_good'],
+        'time_window': tuple(v['key'] for v in events['_meta']['time_window']),
+    }
+    # TODO use operator module later and real meta processing
+    return algorithm
+
+
 def test_load_events():
     events = load_events(pathlib.Path('tests', 'fixtures', 'custom', 'events.json'))
     assert 'data' in events
+
+
+def test_parse_meta():
+    events = load_events(pathlib.Path('tests', 'fixtures', 'custom', 'events.json'))
+    algorithm = parse_meta(events)
+    assert algorithm['is_bad'] is True
+    assert algorithm['is_good'][0]['key'] == 'closed'
+    assert algorithm['is_good'][1]['key'] == 'status'
+    assert algorithm['time_window'] == ('create_at', 'close_at')
