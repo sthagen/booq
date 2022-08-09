a58cae2099cbb6bf4e1810187795132fcb32bf16
Stefan Hagen <stefan@hagen.link>
2022-08-09 22:13:41 +0200

    Linter happy.

--- booq/booq.py
+++ booq/booq.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=expression-not-assigned,line-too-long
 """API of interface operations for calculating binary year to date metrics with monthly cumulative slices.."""
-import os
+# import os
 import sys
 from typing import List, Union
 
 import kdl  # type: ignore
-from first import first
+# from first import first
 
 
 def it_starts_with_silence() -> None:
     """Stub to bootstrap development and survive the make rounds before implementation is in place."""
-    assert os.getenv('DO_NOT_COMPLAIN_IF_THIS_IS_PRESENT_IN_YOUR_ENV', 'all good') == 'all good'
-    assert sys.path > []
+    # assert os.getenv('DO_NOT_COMPLAIN_IF_THIS_IS_PRESENT_IN_YOUR_ENV', 'all good') == 'all good'
+    # assert sys.path > []
     doc = kdl.Document()
     doc.append(
         kdl.Node(
@@ -29,9 +29,9 @@ def it_starts_with_silence() -> None:
             ],
         )
     )
-    ast = kdl.parse(doc)
-    assert next(ast[0]) == 123
-    assert first((1, 2)) == 1
+    _ = kdl.parse(doc)
+    # assert next(ast[0]) == 123
+    # assert first((1, 2)) == 1
 
 
 def main(argv: Union[List[str], None] = None) -> int:
