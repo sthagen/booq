0ba19a6d201de36d77b75617d1d96c9064e7ae08
Stefan Hagen <stefan@hagen.link>
2022-08-09 22:18:30 +0200

    Bumped impl version for initial release

--- booq/__init__.py
+++ booq/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.8.9+parent.3233165e'
-# [[[end]]] (checksum: ec2cc12146ac51673531f0f6ebebef47)
+__version__ = '2022.8.9+parent.389e71f4'
+# [[[end]]] (checksum: 17d4e70a84916ab90bcfcc82821cdea7)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
